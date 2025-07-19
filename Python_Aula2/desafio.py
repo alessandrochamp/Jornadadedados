d4c9a2a4be41dda5ac77d6785370acbdcb056e04
ext.alessandro.champ <ext.alessandro.champoski@lmmobilidade.com.br>
2025-07-19 16:27:04 -0300

    FEAT Class 2 exercises 1-10

--- Python_Aula2/desafio.py
+++ Python_Aula2/desafio.py
@@ -1,31 +1,64 @@
 import math
 
 # #### Inteiros (`int`)
-
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero_01 = int(input("Inserir um numero inteiro: "))
+numero_02 = int(input("Inserir outro numero inteiro: "))
+resultado = numero_01 + numero_02
+print("A soma dos numeros é: {resultado}")
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero_01 = int(input("Inserir um numero inteiro: "))
+resultado = numero_01 % 5
+print("O resto da divisão é: {resultado}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero_01 = int(input("Inserir um numero inteiro: "))
+numero_02 = int(input("Inserir outro numero inteiro: "))
+resultado = numero_01 * numero_02
+print("A multiplicação dos números é: {resultado}")
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 numero_01 = int(input("Inserir um numero inteiro: "))
 numero_02 = int(input("Inserir outro numero inteiro: "))
 resultado = numero_01 // numero_02
-print(resultado)
-
+print("A divisão inteira dos números é: {resultado}")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero_01 = int(input("Inserir um numero inteiro: "))
+resultado = numero_01 * numero_01
+print("A divisão inteira dos números é: {resultado}")
 
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
-# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
-# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
-# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
-# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+numero_01 = float(input("Inserir um numero float: "))
+numero_02 = float(input("Inserir outro numero float: "))
+resultado = numero_01 + numero_02
+print("A adição dos números flutuantes é: {resultado}")
 
-#raio_do_circulo = float(input("Digite o raio: "))
-#area_do_circulo = math.pi * raio_do_circulo ** 2
-# area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
-#print(f"{area_do_circulo:.2f}")
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+numero_01 = float(input("Inserir um numero float: "))
+numero_02 = float(input("Inserir outro numero float: "))
+resultado = (numero_01 + numero_02)/2
+print(resultado)
+
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+base = int(input("Inserir um numero float: "))
+expoente = int(input("Inserir outro numero float: "))
+resultado = base ** expoente
+print("O resultado do exponencial é: {resultado}")
+
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+celsius = int(input("Inserir a temperatura: "))
+fahrenheit = (celsius * 9/5) + 32
+print("A temperatura de {celsius} celsius em fahrenheit é: {fahrenheit}")
+
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+raio_do_circulo = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
+print(f"{area_do_circulo:.2f}")
 
 # #### Strings (`str`)
 
